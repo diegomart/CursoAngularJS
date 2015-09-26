3477c859cab51776c8fab64207de268ff7fcae3b
unknown <judimay18@gmail.com>
2015-09-26 13:22:04 -0500

    Mi nueva practica con AngularJS

---
+++ app.js
@@ -0,0 +1,28 @@
+(function(){
+
+
+	'use strict';
+	angular.module('app',[])
+		.run(function($rootScope){
+
+			$rootScope.message= 'This is a funny message';
+			$rootScope.friends=[{
+
+				"name": "Juan Diego" ,
+				"age": "22"
+			},{
+
+
+				"name": "Fernando",
+				"age": "23"
+			},{
+
+				"name": "Issac",
+				"age": "12"
+			
+			}];
+
+			$rootScope.pillos="Homosexual";
+
+		});
+}());
